e7ab59bf925fbd52c5b4f87542a27c60e2c41c2e
trancongphong <trancongphong@archway.co.jp>
2018-10-03 10:45:51 +0700

    update code for  solution

---
+++ src/Controllers/Api/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sonarproj.Controllers.Api
+{
+    [Route("api/Home")]
+    public class HomeController
+    {
+        [HttpGet()]
+        public int Search()
+        {
+            int c = 1 + 1;
+            return c;
+        }
+    }
+}
---
+++ test/TestTemplate/Controller/Api/HomeControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using sonarproj.Controllers.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestTemplate.Controller.Api
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Search()
+        {
+            HomeController controller = new HomeController();
+            var result =  controller.Search();
+
+            Assert.Equal(2, result);
+        }
+    }
+}
